3ed4088169bb9bbb2305be81aa345fb9a84eb0b3
uros <83064029+MaksimovicUros@users.noreply.github.com>
2026-05-07 23:26:31 +0200

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -35,28 +35,8 @@ const allowedOrigins = (process.env.FRONTEND_URL || "http://localhost:4200")
   .split(",")
   .map((o) => o.trim());
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      const allowedOrigins = (process.env.FRONTEND_URL || "")
-        .split(",")
-        .map((o) => o.trim());
-
-      // allow server-to-server / curl
-      if (!origin) return callback(null, true);
-
-      // allow if match
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      console.log("Blocked CORS origin:", origin);
-      return callback(null, false); // ❗ do NOT throw error
-    },
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-  }),
-);
+app.use(cors());
+app.options("*", cors());
 
 app.use(express.json({ limit: "10kb" }));
 
